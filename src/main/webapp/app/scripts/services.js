67ffc2afa702a1873612e6b8b767279b5aa55891
Pablo Bastidas <pbastidas@conexia.com>
2014-09-09 09:20:35 -0500

    Documentaciond e la función

--- src/main/webapp/app/scripts/services.js
+++ src/main/webapp/app/scripts/services.js
@@ -6,6 +6,14 @@
     function Geo($resource, $http, BaseUrl){
         var geoResource = $resource(BaseUrl + '/geo/:geoId',{geoId:'@_id'});
 
+        /**
+         * funcion dispuesta para realizar consulta a servicios de los puntos mas
+         * cercanos al pointId indicado
+         *
+         * @param pointId id del punto a consultar
+         * @param dibujarCercanos Funcion que recibe un data y debe utilizar la info
+         * @param errorEnTransaccion En caso de error.
+         */
         geoResource.cercanos = function(pointId, dibujarCercanos, errorEnTransaccion){
             $http.get(BaseUrl + '/geo/'+pointId+'/cercanos')
                 .success(dibujarCercanos).error(errorEnTransaccion);
